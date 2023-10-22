f8077cf991ebbe4221219770308debf1d65929ed
EricOfficial <eric.naeser@gmx.de>
2023-10-22 13:35:06 +0900

    rename file and upload homework 1 pdf

--- heat-conduction-equation.py
+++ heat-conduction-equation.py
@@ -2,6 +2,10 @@ import math
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+np.seterr(divide = 'ignore')
 
 
 def initialize_finite_difference_grid(max_k, dk, nx, ny, lower_t, upper_t, left_t, right_t):
@@ -32,7 +36,7 @@ def initialize_grid(nx, ny, lower_t, upper_t, left_t, right_t):
     return T
 
 
-def calculate_jacobi(T, max_k, dk, nx, ny, dx, dy, alpha):
+def calculate_jacobi(T, nx, ny, dx, dy, alpha):
 
     # Initialize loop parameters
     current_error = decay_error = 10
@@ -64,9 +68,11 @@ def calculate_jacobi(T, max_k, dk, nx, ny, dx, dy, alpha):
     plt.loglog(res, "-b")
     plt.xlabel('number of iteration - Gauss-seidell')
     plt.ylabel('Residual')
+    plt.grid()
     plt.title('Convergence History')
     plt.legend(["Number of Iteration= {:3d} ".format(k)])
     plt.savefig("homework1-task1-plots/jacobi-error-convergence-{}.png".format(nx), format="png")
+    plt.clf()
 
     return T
 
@@ -103,9 +109,11 @@ def calculate_gauss_seidel(T, nx, ny, dx, dy, alpha):
     plt.loglog(res, "-b")
     plt.xlabel('number of iteration - Gauss-seidell')
     plt.ylabel('Residual')
+    plt.grid()
     plt.title('Convergence History')
     plt.legend(["Number of Iteration= {:3d} ".format(k)])
     plt.savefig("homework1-task1-plots/gs-error-convergence-{}.png".format(nx), format="png")
+    plt.clf()
 
     return T
 
@@ -143,11 +151,67 @@ def calculate_sor(T, nx, ny, dx, dy, alpha):
     plt.loglog(res, "-b")
     plt.xlabel('number of iteration - Successive over-relaxation')
     plt.ylabel('Residual')
+    plt.grid()
     plt.title('Convergence History')
     plt.legend(["Number of Iteration= {:3d} ".format(k)])
     plt.savefig("homework1-task1-plots/sor-error-convergence-{}.png".format(nx), format="png")
+    plt.clf()
 
     return T
+
+
+def calculate_exact(T, nx, ny, dx, dy):
+    equation_steps = 200
+
+    for i in range(1, (nx - 1)):
+        x = i * dx
+        for j in range(1, (ny - 1)):
+            y = j * dy
+
+            sum_loop = 0
+            for n in range(1, equation_steps):
+
+                factor1 = ((-1)**(n+1)+1)/n
+                factor2 = math.sin(n * math.pi * x)
+                factor3 = (math.sinh(n * math.pi * y)) / (math.sinh(n * math.pi))
+                sum_loop += factor1 * factor2 * factor3
+
+            T[j, i] = (2 / math.pi) * sum_loop
+
+    return T
+
+
+def calculate_mean_absolute_error(T_solved, T_exact, nx, ny):
+    abs_diff_list = []
+    # print(nx, ny)
+    for i in range(nx):
+        for j in range(ny):
+            abs_diff = abs(T_exact[i, j] - T_solved[i, j])
+            # print(T_exact[i, j], T_solved[i ,j], abs_diff)
+            abs_diff_list.append(abs_diff)
+
+    mae = sum(abs_diff_list) / len(abs_diff_list)
+    return mae
+
+
+def plot_contour(X, Y, T, solver, size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cs = ax.contourf(X, Y, T[:, :], levels=20, cmap='gist_rainbow_r')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    fig.colorbar(cs, ticks=[i for i in np.arange(0.0, 1.05, 0.05)], label="Phi")
+    plt.savefig("homework1-task1-plots/contour-{}-{}.png".format(solver, size), format="png")
+    plt.clf()
+
+
+def plot_centerline(x, y, solver, size):
+    plt.plot(x, y)
+    plt.title("Centerline plot for {} with grid of size {}".format(solver, size))
+    plt.xlabel("Y")
+    plt.ylabel("Phi")
+    plt.savefig("homework1-task1-plots/centerline-{}-{}.png".format(solver, size), format="png")
+    plt.clf()
 
 
 def run_main_logic(solver, size):
@@ -185,56 +249,60 @@ def run_main_logic(solver, size):
     Y = np.linspace(0, W, ny, endpoint=True)
     X, Y = np.meshgrid(X, Y)
 
-    T = initialize_grid(nx, ny, lower_t, upper_t, left_t, right_t)
+    T_init = initialize_grid(nx, ny, lower_t, upper_t, left_t, right_t)
+
     if solver == "jacobi":
-        T = calculate_jacobi(T, max_k, dk, nx, ny, dx, dy, alpha)
+        T_solved = calculate_jacobi(T_init, nx, ny, dx, dy, alpha)
     elif solver == "gauss-seidel":
-        T = calculate_gauss_seidel(T, nx, ny, dx, dy, alpha)
+        T_solved = calculate_gauss_seidel(T_init, nx, ny, dx, dy, alpha)
     elif solver == "sor":
-        T = calculate_sor(T, nx, ny, dx, dy, alpha)
+        T_solved = calculate_sor(T_init, nx, ny, dx, dy, alpha)
+    else:
+        raise TypeError('Solver not valid!')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cs = ax.contourf(X, Y, T[:, :], levels=20, cmap='gist_rainbow_r')
-    ax.set_xlabel('X [m]')
-    ax.set_ylabel('Y [m]')
-    fig.colorbar(cs, ticks=[i for i in np.arange(0.0, 1.05, 0.05)])
-    plt.savefig("homework1-task1-plots/contour-{}-{}.png".format(solver, size), format="png")
-    plt.show()
+    plot_centerline(np.linspace(0, W, ny, endpoint=True), T_solved[:, int(nx / 2)], solver, size)
+    plot_contour(X, Y, T_solved, solver, size)
 
-    return 5
+    T_exact = calculate_exact(T_init, nx, ny, dx, dy)
+    #print("solved\n", pd.DataFrame(T_solved))
+    #print("exact\n", pd.DataFrame(T_exact))
+    plot_contour(X, Y, T_exact, "Exact", size)
+
+    mae = calculate_mean_absolute_error(T_solved, T_exact, nx, ny)
+    return mae
 
 
 def config_search_space():
 
-    x_values = [5, 10, 20, 50, 100]
+    # Initialize variables
+    x_values = [20, 50]
     solver_errors = {}
+
+    # Iterate over variable combinations
     for solver in ["jacobi", "gauss-seidel", "sor"]:
         print(solver)
         errors = []
         for size in x_values:
             error = run_main_logic(solver, size)
-            if solver == "gauss-seidel":
-                error += 1
-            elif solver == "sor":
-                error += 2
             errors.append(error)
         solver_errors[solver] = errors
 
     x_values = [1/i for i in x_values]
 
+    # Create multiple lines for plot
     plt.plot(x_values, solver_errors["jacobi"], label="Jacobi")
     plt.plot(x_values, solver_errors["gauss-seidel"], label="Gauss Seidel")
     plt.plot(x_values, solver_errors["sor"], label="Successive over-relaxation")
 
+    # Edit plot settings
     plt.xlabel("Delta X")
     plt.ylabel("Error")
     plt.xscale("log")
-    plt.ylim([0, 10])
     plt.legend()
+    plt.grid()
     plt.title("Error of matrix solvers with different grid densities")
     plt.savefig("homework1-task1-plots/grid-density-errors.png", format="png")
-    plt.show()
+    plt.clf()
 
 
 def manual_run(solver, size):
