6c234b1efd3ec1eb8f0b58d297a2f0bc0b042c96
EricOfficial <eric.naeser@gmx.de>
2023-10-29 15:01:47 +0900

    implemented explicit euler

--- convection-diffusion-problem.py
+++ convection-diffusion-problem.py
@@ -32,6 +32,73 @@ def initialize_unsteady_grid(max_k, nx, ny, lower_t, upper_t, left_t, right_t):
     return T
 
 
+def calculate_finite_volume(xnode, ynode, xcell, ycell, dx, dy, i, j, gamma, rho):
+    ue = xnode[i]
+    uw = xnode[i - 1]
+    vn = (-1) * ynode[j]
+    vs = (-1) * ynode[j - 1]
+
+    # Convection term m attributes
+    me = rho * ue * dy
+    mw = rho * uw * dy
+    mn = rho * vn * dx
+    ms = rho * vs * dx
+
+    # Convection term lambda attributes
+    le = (xnode[i] - xcell[i]) / (xcell[i + 1] - xcell[i])
+    lw = (xcell[i - 1] - xcell[i]) / (xcell[i - 1] - xcell[i])
+    ln = (ycell[j] - ycell[j]) / (ycell[j + 1] - ycell[j])
+    ls = (ycell[j - 1] - ycell[j]) / (ycell[j - 1] - ycell[j])
+
+    # Convection term with central difference
+    ace = me * le
+    acw = mw * lw
+    acn = mn * ln
+    acs = ms * ls
+    acp = -(ace + acw + acn + acs)
+
+    # Diffusion term with central difference
+    ade = -(gamma * dy) / (xcell[i + 1] - xcell[i])
+    adw = -(gamma * dy) / (xcell[i] - xcell[i - 1])
+    adn = -(gamma * dx) / (ycell[j + 1] - ycell[j])
+    ads = -(gamma * dx) / (ycell[j] - ycell[j - 1])
+    adp = -(ade + adw + adn + ads)
+
+    # Add convection and diffusion terms
+    a_e = ace - ade
+    a_w = acw - adw
+    a_n = acn - adn
+    a_s = acs - ads
+    a_p = acp - adp
+
+    return a_e, a_w, a_n, a_s, a_p
+
+
+def calculate_unsteady_finite_volume(nx, ny, xnode, ynode, xcell, ycell, u, v, gamma, rho, dt):
+    a_w = np.zeros((nx, ny))
+    a_e = np.zeros((nx, ny))
+    a_n = np.zeros((nx, ny))
+    a_s = np.zeros((nx, ny))
+    a_p = np.zeros((nx, ny))
+
+    for i in range(1, nx + 1):
+        dx = xnode[i] - xnode[i - 1]
+        for j in range(1, ny + 1):
+            dy = ynode[j] - ynode[j - 1]
+            delx_e = xcell[i + 1] - xcell[i]
+            delx_w = xcell[i] - xcell[i - 1]
+            dely_n = ycell[j + 1] - ycell[j]
+            dely_s = ycell[j] - ycell[j - 1]
+            a_w[i - 1, j - 1] = gamma * dy / delx_w - min(rho * u[i - 1, j] * dy * (-1), 0)
+            a_e[i - 1, j - 1] = gamma * dy / delx_e - min(rho * u[i, j] * dy, 0)
+            a_n[i - 1, j - 1] = gamma * dx / dely_n - min(rho * v[i, j] * dx, 0)
+            a_s[i - 1, j - 1] = gamma * dx / dely_s - min(rho * v[i, j - 1] * dx * (-1), 0)
+            ap0 = rho * dx * dy / dt
+            a_p[i - 1, j - 1] = ap0
+
+    return a_e, a_w, a_n, a_s, a_p, ap0
+
+
 def calculate_gauss_seidel(T, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha):
 
     # Initialize loop parameters
@@ -54,65 +121,35 @@ def calculate_gauss_seidel(T, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha)
         for i in range(1, (nx - 1)):
             for j in range(1, (ny - 1)):
 
-                u = xnode[i]
-                v = -ynode[j]
+                a_e, a_w, a_n, a_s, a_p = calculate_finite_volume(xnode, ynode, xcell, ycell, dx, dy, i, j, gamma, rho)
 
-                # Convection term m attributes
-                me = rho * u * dy
-                mw = rho * u * dy
-                mn = rho * v * dx
-                ms = rho * v * dx
-
-                # Convection term lambda attributes
-                le = (xnode[i] - xcell[i]) / (xcell[i + 1] - xcell[i])
-                lw = (xcell[i - 1] - xcell[i]) / (xcell[i - 1] - xcell[i])
-                ln = (ycell[j] - ycell[j]) / (ycell[j + 1] - ycell[j])
-                ls = (ycell[j - 1] - ycell[j]) / (ycell[j - 1] - ycell[j])
-
-                # Convection term with central difference
-                ace = me * le
-                acw = mw * lw
-                acn = mn * ln
-                acs = ms * ls
-                acp = -(ace + acw + acn + acs)
-
-                # Diffusion term with central difference
-                ade = -(gamma * dy) / (xcell[i + 1] - xcell[i])
-                adw = -(gamma * dy) / (xcell[i] - xcell[i - 1])
-                adn = -(gamma * dx) / (ycell[j + 1] - ycell[j])
-                ads = -(gamma * dx) / (ycell[j] - ycell[j - 1])
-                adp = -(ade + adw + adn + ads)
-
-                # Add convection and diffusion terms
-                a_e = ace + ade
-                a_w = acw + adw
-                a_n = acn + adn
-                a_s = acs + ads
-                a_p = acp + adp
-
+                # cell value next to west boundary
                 if i == 1:
-                    Tw = 1 - ycell[j]
+                    tw = 1 - ycell[j]
                 else:
-                    Tw = T[i - 1, j]
+                    tw = T[i - 1, j]
 
-                if i == nx:
-                    Te = T[i,j]
+                # cell value next to east boundary
+                if i == nx - 1:
+                    te = T[i, j]
                 else:
-                    Te = T[i + 1, j]
+                    te = T[i + 1, j]
 
+                # cell value next to south boundary
                 if j == 1:
-                    Ts = T[i, j]
+                    ts = T[i, j]
                 else:
-                    Ts = T[i, j - 1]
+                    ts = T[i, j - 1]
 
-                if j == ny:
-                    Tn = 0
+                # cell value next to north boundary
+                if j == ny - 1:
+                    tn = 0
                 else:
-                    Tn = T[i, j + 1]
+                    tn = T[i, j + 1]
 
                 # Calculate T
-                tij = (a_e * Te + a_w * Tw + a_n * Tn + a_s * Ts) / a_p
-                T[i, j] = alpha * tij + T_decay[i, j]
+                tij = (a_e * te + a_w * tw + a_n * tn + a_s * ts) / a_p
+                T[i, j] = tij
 
         # Update loop parameters
         current_error = abs(np.linalg.norm(T) - np.linalg.norm(T_decay))
@@ -145,46 +182,36 @@ def calculate_sor(T, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha):
         # Calculate with SOR method
         for i in range(1, (nx - 1)):
             for j in range(1, (ny - 1)):
-                u = xnode[i]
-                v = -ynode[j]
 
-                # Convection term m attributes
-                me = rho * u * dy
-                mw = rho * u * dy
-                mn = rho * v * dx
-                ms = rho * v * dx
+                a_e, a_w, a_n, a_s, a_p = calculate_finite_volume(xnode, ynode, xcell, ycell, dx, dy, i, j, gamma, rho)
 
-                # Convection term lambda attributes
-                le = (xnode[i] - xcell[i]) / (xcell[i + 1] - xcell[i])
-                lw = (xcell[i - 1] - xcell[i]) / (xcell[i - 1] - xcell[i])
-                ln = (ycell[j] - ycell[j]) / (ycell[j + 1] - ycell[j])
-                ls = (ycell[j - 1] - ycell[j]) / (ycell[j - 1] - ycell[j])
+                # cell value next to west boundary
+                if i == 1:
+                    tw = 1 - ycell[j]
+                else:
+                    tw = T[i - 1, j]
 
-                # Convection term with central difference
-                ace = me * le
-                acw = mw * lw
-                acn = mn * ln
-                acs = ms * ls
-                acp = -(ace + acw + acn + acs)
+                # cell value next to east boundary
+                if i == nx - 1:
+                    te = T_decay[i, j]
+                else:
+                    te = T_decay[i + 1, j]
 
-                # Diffusion term with central difference
-                ade = -(gamma * dy) / (xcell[i + 1] - xcell[i])
-                adw = -(gamma * dy) / (xcell[i] - xcell[i - 1])
-                adn = -(gamma * dx) / (ycell[j + 1] - ycell[j])
-                ads = -(gamma * dx) / (ycell[j] - ycell[j - 1])
-                adp = -(ade + adw + adn + ads)
+                # cell value next to south boundary
+                if j == 1:
+                    ts = T[i, j]
+                else:
+                    ts = T[i, j - 1]
 
-                # Add convection and diffusion terms
-                a_e = ace + ade
-                a_w = acw + adw
-                a_n = acn + adn
-                a_s = acs + ads
-                a_p = acp + adp
+                # cell value next to north boundary
+                if j == ny - 1:
+                    tn = 0
+                else:
+                    tn = T_decay[i, j + 1]
 
                 # Calculate T
-                tij = (a_e * T_decay[i + 1, j] + a_w * T[i - 1, j] + a_n * T_decay[i, j + 1] + a_s * T[
-                    i, j - 1]) / a_p
-                T[i, j] = (1 - omega) * T_decay[i, j] + omega * (alpha * tij + T[i, j])
+                tij = (a_e * te + a_w * tw + a_n * tn + a_s * ts) / a_p
+                T[i, j] = (1 - omega) * T_decay[i, j] + omega * (tij)
 
         # Update loop parameters
         current_error = abs(np.linalg.norm(T) - np.linalg.norm(T_decay))
@@ -195,77 +222,58 @@ def calculate_sor(T, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha):
     return T, convergence
 
 
-def calculate_explicit_euler(T, max_k, dk, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha):
-
-    # Initialize loop parameters
-    current_error = decay_error = 10
-    k = 0
-    convergence = np.array([], float)
-    T_decay = np.copy(T)
-
-    gamma = 0.001
-    rho = 1.0
-
-    for k in range(0, max_k - 1):
-
-        # Print error every log10 steps
-        if int(math.log10(current_error)) != int(math.log10(decay_error)):
-            print("Current error", current_error)
-        decay_error = current_error
-
-        # Calculate with Gauss Seidel method
-        for i in range(1, (nx - 1)):
-            for j in range(1, (ny - 1)):
-                u = xnode[i]
-                v = -ynode[j]
-
-                # Convection term m attributes
-                me = rho * u * dy
-                mw = rho * u * dy
-                mn = rho * v * dx
-                ms = rho * v * dx
-
-                # Convection term lambda attributes
-                le = (xnode[i] - xcell[i]) / (xcell[i + 1] - xcell[i])
-                lw = (xcell[i - 1] - xcell[i]) / (xcell[i - 1] - xcell[i])
-                ln = (ycell[j] - ycell[j]) / (ycell[j + 1] - ycell[j])
-                ls = (ycell[j - 1] - ycell[j]) / (ycell[j - 1] - ycell[j])
-
-                # Convection term with central difference
-                ace = me * le
-                acw = mw * lw
-                acn = mn * ln
-                acs = ms * ls
-                acp = -(ace + acw + acn + acs)
-
-                # Diffusion term with central difference
-                ade = -(gamma * dy) / (xcell[i + 1] - xcell[i])
-                adw = -(gamma * dy) / (xcell[i] - xcell[i - 1])
-                adn = -(gamma * dx) / (ycell[j + 1] - ycell[j])
-                ads = -(gamma * dx) / (ycell[j] - ycell[j - 1])
-                adp = -(ade + adw + adn + ads)
-
-                # Add convection and diffusion terms
-                a_e = ace + ade
-                a_w = acw + adw
-                a_n = acn + adn
-                a_s = acs + ads
-                a_p = acp + adp
-
-                # Calculate T
-                dv = (xnode[i] - xnode[i - 1]) * (ynode[j] - ynode[j - 1])
-                ai_phi = a_p * T_decay[k - 1, i, j] + a_e * T_decay[k - 1, i + 1, j] + a_w * T_decay[k - 1, i - 1, j] \
-                         + a_n * T_decay[k - 1, i, j + 1] + a_s * T_decay[k - 1, i, j - 1]
-                T[k, i, j] = T_decay[k - 1, i, j] + (dk / dv / gamma) * (ai_phi - a_p * T_decay[k - 1, i, j])
-
-        if k != 0:
-            current_error = abs(np.linalg.norm(T[k, :, :]) - np.linalg.norm(T[k - 1, :, :]))
-        convergence = np.append(convergence, current_error)
-
-    return T, convergence
+def calculate_explicit_euler(T, max_k, dk, nx, ny, ymax, ycell, a_e, a_w, a_n, a_s, a_p, ap0):
+    for k in np.arange(0, max_k, dk):
+        T_decay = T.copy()
+        for i in range(1, nx + 1):
+            for j in range(1, ny + 1):
+                if i == 1:
+                    t_w = 1 - ycell[j] / ymax
+                else:
+                    t_w = T_decay[i - 1, j]
+                if i == nx + 1:
+                    t_e = T_decay[i, j]
+                else:
+                    t_e = T_decay[i + 1, j]
+                if j == 1:
+                    t_s = T_decay[i, j]
+                else:
+                    t_s = T_decay[i, j - 1]
+                if j == ny + 1:
+                    t_n = 0.0
+                else:
+                    t_n = T_decay[i, j + 1]
+                t_p = T_decay[i, j]
+                T[i, j] = a_e[i - 1, j - 1] * t_e + a_w[i - 1, j - 1] * t_w + a_n[i - 1, j - 1] * t_n + a_s[i - 1, j - 1] * t_s + (
+                        ap0 - a_e[i - 1, j - 1] - a_w[i - 1, j - 1] - a_n[i - 1, j - 1] - a_s[i - 1, j - 1]) * t_p
+                T[i, j] = T[i, j] / a_p[i - 1, j - 1]
+    return T
 
 
 def plot_contour(X, Y, T, solver, size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cs = ax.contourf(X, Y, T[:, :], levels=20, cmap='viridis_r')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    fig.colorbar(cs, ticks=[i for i in np.arange(0.0, 1.05, 0.05)], label="Phi")
+    plt.savefig("homework1-task2-plots/contour-{}-{}.png".format(solver, size), format="png")
+    plt.clf()
+
+
+def plot_unsteady_contour(X, Y, T, solver, size, nx, ny, ycell, ymax):
+
+    for i in range(nx + 2):
+        for j in range(ny + 2):
+            if i == 0:
+                T[i, j] = 1 - ycell[j] / ymax
+            elif i == nx + 1:
+                T[i, j] = T[i - 1, j]
+            elif j == 0:
+                T[i, j] = T[i, j + 1]
+            elif j == ny + 1:
+                T[i, j] = 0.0
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     cs = ax.contourf(X, Y, T[:, :], levels=20, cmap='viridis_r')
@@ -330,7 +338,7 @@ def run_main_logic(solver, size):
     elif solver == "sor":
         T_solved, convergence = calculate_sor(T_init, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha)
     elif solver == "explicit":
-        T_solved, convergence = calculate_explicit_euler(T_init, max_k, dk, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha)
+        T_solved = calculate_explicit_euler(T_init, max_k, dk, nx, ny, xnode, ynode, xcell, ycell, dx, dy, alpha)
     else:
         raise TypeError('Solver not valid!')
 
@@ -340,6 +348,51 @@ def run_main_logic(solver, size):
         plot_contour(X, Y, T_solved[max_k - 1, :, :], solver, size)
     else:
         raise TypeError('Solver not valid!')
+
+
+def run_unsteady_main_logic(solver, size):
+    nx = ny = size
+
+    B = np.zeros((1, nx * ny))
+    T = np.zeros((nx + 2, ny + 2))
+
+    t0 = 0
+    t1 = 1
+
+    dx = 1 / nx
+    dy = 1 / ny
+
+    xmax = ymax = 1
+    xmin = ymin = 0
+
+    gamma = 0.1
+    rho = 1.2
+
+    dk = 0.02
+    max_k = 5.0
+
+    xnode = np.linspace(xmin, xmax, nx + 1)
+    xcell = np.linspace(xmin + dx / 2, xmax - dx / 2, nx)
+    xcell = np.concatenate(([xmin], xcell, [xmax]))
+
+    ynode = np.linspace(ymin, ymax, ny + 1)
+    ycell = np.linspace(ymin + dy / 2, ymax - dy / 2, ny)
+    ycell = np.concatenate(([ymin], ycell, [ymax]))
+
+    u = np.zeros((nx + 1, nx + 1))
+    v = np.zeros((ny + 1, ny + 1))
+
+    for i in range(nx + 1):
+        for j in range(ny + 1):
+            u[i, j] = xnode[i]
+
+    for i in range(nx + 1):
+        for j in range(ny + 1):
+            v[i, j] = -ynode[j]
+
+    a_e, a_w, a_n, a_s, a_p, ap0 = calculate_unsteady_finite_volume(nx, ny, xnode, ynode, xcell, ycell, u, v, gamma, rho, dk)
+    T = calculate_explicit_euler(T, max_k, dk, nx, ny, ymax, ycell, a_e, a_w, a_n, a_s, a_p, ap0)
+    plot_unsteady_contour(xcell, ycell, T, solver, size, nx, ny, ycell, ymax)
 
 
 def config_search_space():
@@ -361,40 +414,24 @@ def config_search_space():
             print(" ")
 
 
+def config_unsteady_search_space():
+    # Initialize variables
+    x_values = [5, 10]
+    solver_results = {}
+
+    # Iterate over variable combinations
+    for solver in ["explicit"]:
+        solver_results[solver] = {}
+        for size in x_values:
+            print("----------------------")
+            print("Current solver is {} with grid size of {}".format(solver, size))
+            print("----------------------")
+
+            run_unsteady_main_logic(solver, size)
+
+            print(" ")
+
+
 if __name__ == '__main__':
-    config_search_space()
-
-
-# Diffusion term with central difference
-"""ade = -(2 * gamma) / ((X[i + 1] - X[i - 1]) * (X[i + 1] - X[i]))
-adw = -(2 * gamma) / ((X[i + 1] - X[i - 1]) * (X[i] - X[i - 1]))
-adn = -(2 * gamma) / ((Y[j + 1] - Y[j - 1]) * (Y[j + 1] - Y[j]))
-ads = -(2 * gamma) / ((Y[j + 1] - Y[j - 1]) * (Y[j] - Y[j - 1]))
-adp = -(ade + adw + adn + ads)"""
-
-# Convection term with central difference
-"""ace = -(roh * X[i]) / (X[i + 1] - X[i - 1])
-acw = (roh * X[i]) / (X[i + 1] - X[i - 1])
-acn = -(roh * Y[i]) / (Y[i + 1] - Y[i - 1])
-acs = (roh * Y[i]) / (Y[i + 1] - Y[i - 1])
-acp = -(ace + acw + acn + acs)"""
-
-#part1 = (1 / (2 * dx * dy)) * (T_decay[i + 1, j] + T_decay[i - 1, j] + T_decay[i, j + 1] + T_decay[i, j - 1])
-#part2 = (roh * X[i] * dy / (2 * dx)) * (T_decay[i + 1, j] - T_decay[i - 1, j])
-#part3 = (roh * -Y[i] * dx / (2 * dy)) * (T_decay[i, j + 1] - T_decay[i, j - 1])
-""""a = (T_decay[i + 1, j] - 2 * T_decay[i, j] + T[i - 1, j]) / dx**2
-b = (T_decay[i, j + 1] - 2 * T_decay[i, j] + T[i, j - 1]) / dy**2
-part2 = a * X[i]
-part3 = b * -Y[i]
-part4 = alpha * gamma * (a + b)
-T[i, j] = part2 + part3 - part4"""
-
-
-""" video approach
-ace = gamma - rho * u * dx / 2
-acw = gamma + rho * u * dx / 2
-acn = gamma - rho * v * dy / 2
-acs = gamma + rho * v * dy / 2
-acp = rho * u * dx / 2 - rho * u * dx / 2 + rho * v * dy / 2 - rho * v * dy / 2 + 4 * gamma
-T[i, j] = (ace * T[i + 1, j] + acw * T[i - 1, j] + acn * T_decay[i, j + 1] + acs * T_decay[i, j - 1]) / acp
-"""
+    #config_search_space()
+    config_unsteady_search_space()
