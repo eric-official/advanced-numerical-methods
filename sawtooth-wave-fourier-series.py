6887973ba5514a2c71fd3c5c9512ce7e291f9e35
EricOfficial <eric.naeser@gmx.de>
2023-11-23 14:37:06 +0900

    added plot storage

--- sawtooth-wave-fourier-series.py
+++ sawtooth-wave-fourier-series.py
@@ -2,21 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import simps
 from scipy import fftpack
+import os
 
 
 # Generation of Sawtooth function
 def falling_sawtooth_wave(t, frequency, amplitude=1.0):
-    """
-    Generate a falling sawtooth wave between -1 and 1.
-
-    Parameters:
-    - t: Time array.
-    - frequency: Frequency of the sawtooth wave.
-    - amplitude: Amplitude of the sawtooth wave (default is 1.0).
-
-    Returns:
-    - Array containing the values of the falling sawtooth wave at each time point.
-    """
     sawtooth_wave_values = amplitude * (- 2 * (t * frequency - np.floor(0.5 + t * frequency)))
     return sawtooth_wave_values
 
@@ -25,7 +15,7 @@ def main():
     L = 1  # Periodicity of the periodic function f(x)
     frequency = 1  # No of waves in time period L
     samples = 256
-    terms = 50
+    k = 50 # Number of terms in the fourier series
 
     # Create a time array from 0 to 1 seconds with a given sampling rate (e.g., 1000 samples per second).
     t = np.arange(0, 1, 1 / samples)
@@ -41,7 +31,9 @@ def main():
 
     # Sum of the series
     h = a0 / 2. + sum([an(k) * np.cos(2. * np.pi * k * x / L) + bn(k) * np.sin(2. * np.pi *
-                                                                               k * x / L) for k in range(1, terms + 1)])
+                                                                               k * x / L) for k in range(1, k + 1)])
+
+    # Repeat the sawtooth wave 4 times
     x4 = np.linspace(0, 4, 4 * samples, endpoint=False)
     y4 = np.tile(y, 4)
     h4 = np.tile(h, 4)
@@ -53,8 +45,7 @@ def main():
     plt.title("Sawtooth wave signal analysis with Fouries series")
     plt.grid()
     plt.ylim([-1.5, 1.5])
-    plt.savefig("sawtooth.png")
-    plt.show()
+    plt.savefig("homework2-task3-plots/sawtooth-wave.png", format="png")
     plt.clf()
 
     # Plot fourier series expansion
@@ -64,8 +55,7 @@ def main():
     plt.title("Sawtooth wave signal analysis with Fouries series")
     plt.grid()
     plt.ylim([-1.5, 1.5])
-    plt.savefig("sawtooth.png")
-    plt.show()
+    plt.savefig("homework2-task3-plots/sawtooth-fourier.png", format="png")
     plt.clf()
 
     # Do fast fourier transform
@@ -74,37 +64,46 @@ def main():
     # Calculate Fast Fourier Transform
     amplitude = np.abs(fft)/samples
     power = amplitude ** 2
-    angle = np.angle(fft)
     sample_freq = fftpack.fftfreq(fft.size, d=1/samples)
-    amp_freq = np.array([sample_freq, amplitude])
-    amp_position = amp_freq[0, :].argmax()
-    peak_freq = amp_freq[1, amp_position]
 
     # Plot real part of FFT
-    plt.plot(sample_freq[:int(samples/2)], np.imag(fft[:int(samples/2)]))
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude')
-    plt.title('Real part of FFT')
+    plt.plot(sample_freq[1:int(samples / 2)], np.real(fft[1:int(samples / 2)]))
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Amplitude")
+    plt.title("Real part of FFT")
     plt.grid()
-    plt.show()
+    plt.savefig("homework2-task3-plots/fft-real.png", format="png")
+    plt.clf()
+
+    # Plot imaginary part of FFT
+    plt.plot(sample_freq[:int(samples/2)], np.imag(fft[:int(samples/2)]))
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Amplitude")
+    plt.title("Real part of FFT")
+    plt.grid()
+    plt.savefig("homework2-task3-plots/fft-imaginary.png", format="png")
     plt.clf()
 
     # Plot Power Spectrum
     plt.plot(sample_freq, power)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Power')
-    plt.title('Power spectrum')
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Power")
+    plt.title("Power spectrum")
     plt.grid()
-    plt.show()
+    plt.savefig("homework2-task3-plots/fft-power-spectrum.png", format="png")
     plt.clf()
 
     # Plot Frequency
     plt.plot(sample_freq)
     plt.ylabel("Frequency [Hz]")
     plt.grid()
-    plt.show()
+    plt.savefig("homework2-task3-plots/fft-frequency.png", format="png")
     plt.clf()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # if the demo_folder directory is not present then create it
+    if not os.path.exists("homework2-task3-plots"):
+        os.makedirs("homework2-task3-plots")
+
     main()
